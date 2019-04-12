df7a2c0e1e96e294f98793f6b2d737e2f891c87c
Ben Daniels <benbanj@hotmail.com>
2019-04-12 17:46:22 +0100

    Completing timer

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,12 @@
 class Timer
-  #write your code here
+  attr_accessor :seconds
+
+  def initialize
+    @seconds = 0
+  end
+
+  def time_string
+    time = Time.new(2019)
+    (time + @seconds).strftime("%H:%M:%S")
+  end
 end
