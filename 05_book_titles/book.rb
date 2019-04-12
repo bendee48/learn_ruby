fdbe5d6097cd67e55542b75bd3381b171736297d
Ben Daniels <benbanj@hotmail.com>
2019-04-12 17:31:54 +0100

    Comleting book_titles.rb

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,20 @@
 class Book
-# write your code here
+  attr_accessor :title
+
+  def initialize
+    @title
+  end
+
+  def title
+    articles_array = %w(the and in of a an)
+    words = @title.split.map.with_index do |word, index|
+      if articles_array.include?(word) && index != 0
+        word
+      else
+        word.capitalize
+      end
+    end
+    words.join(" ")
+  end
+
 end
