3312676b41ba60a55048eb2f89e5ca7deaa97fc6
Ben Daniels <benbanj@hotmail.com>
2019-04-12 14:46:01 +0100

    Completing calculator.rb

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,23 @@
-#write your code here
+def add(num1, num2)
+  num1 + num2
+end
+
+def subtract(num1, num2)
+  num1 - num2
+end
+
+def sum(numbers)
+  numbers.sum
+end
+
+def multiply(*numbers)
+  numbers.reduce { |total, num| total * num }
+end
+
+def power(num, power_num)
+  num ** power_num
+end
+
+def factorial(num)
+  (1..num).to_a.reduce { |total, num| total * num } || 1
+end
